5d9bfd121fd4c5553d3926197efa8fcb62208352
Jacob Ferdinandsen <Jacobferdinandsen@gmail.com>
2015-04-22 14:22:59 +0200

    lavet mulighed for at trykke på "H" for at komme til highscore siden

--- client/app/game/js/menu.js
+++ client/app/game/js/menu.js
@@ -4,6 +4,7 @@
     function Menu() {
         this.titleTxt = null;
         this.startTxt = null;
+        this.highscoreTxt = null;
     }
 
     Menu.prototype = {
@@ -11,23 +12,36 @@
         create: function () {
             var x = this.game.width / 2,
                 y = this.game.height / 2,
-                key = this.game.input.keyboard.addKey(Phaser.Keyboard.N);
+                startGameKey = this.game.input.keyboard.addKey(Phaser.Keyboard.N),
+                highscoreKey = this.game.input.keyboard.addKey(Phaser.Keyboard.H);
 
             this.titleTxt = this.add.bitmapText(x, y, 'gul', 'Dr. Evils Dangerous Bunnies', 48);
             this.titleTxt.align = 'center';
             this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
 
-            y = y + this.titleTxt.height + 5;
+            y = y + this.titleTxt.height + 50;
             this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'Press "N" to start the game');
             this.startTxt.align = 'center';
             this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
+
+            y = y + this.startTxt.height + 20;
+            this.highscoreTxt = this.add.bitmapText(x, y, 'minecraftia', 'Press "H" for highscore page');
+            this.highscoreTxt.align = 'center';
+            this.highscoreTxt.x = this.game.width / 2 - this.highscoreTxt.textWidth / 2;
+
             //            Legacy! fra Lars 
             //            this.input.onDown.add(this.onDown, this);
-            key.onDown.add(this.startGame, this);
+            startGameKey.onDown.add(this.startGame, this);
+            highscoreKey.onDown.add(this.highscore,this);
+            
         },
         startGame: function () {
             this.game.state.start('game');
         },
+        
+        highscore: function(){
+            window.location = 'http://google.com';
+        }
 
         //        Legacy! fra Lars
         //        onDown: function () {
