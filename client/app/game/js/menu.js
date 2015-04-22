912a453e031a268a2893f37c6c688f1e3ba3715f
Jacob Ferdinandsen <Jacobferdinandsen@gmail.com>
2015-04-22 14:13:38 +0200

    preloaded fonts - added fonts - ændret startknap til "n" ændret forside teksten

--- client/app/game/js/menu.js
+++ client/app/game/js/menu.js
@@ -1,40 +1,41 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  function Menu() {
-    this.titleTxt = null;
-    this.startTxt = null;
-  }
-
-  Menu.prototype = {
-
-    create: function () {
-      var x = this.game.width / 2
-        , y = this.game.height / 2;
-
-
-      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'Ferdi rulez' );
-      this.titleTxt.align = 'center';
-      this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
-
-      y = y + this.titleTxt.height + 5;
-      this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'START');
-      this.startTxt.align = 'center';
-      this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
-
-      this.input.onDown.add(this.onDown, this);
-    },
-
-    update: function () {
-
-    },
-
-    onDown: function () {
-      this.game.state.start('game');
+    function Menu() {
+        this.titleTxt = null;
+        this.startTxt = null;
     }
-  };
 
-  window['pixione'] = window['pixione'] || {};
-  window['pixione'].Menu = Menu;
+    Menu.prototype = {
+
+        create: function () {
+            var x = this.game.width / 2,
+                y = this.game.height / 2,
+                key = this.game.input.keyboard.addKey(Phaser.Keyboard.N);
+
+            this.titleTxt = this.add.bitmapText(x, y, 'gul', 'Dr. Evils Dangerous Bunnies', 48);
+            this.titleTxt.align = 'center';
+            this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
+
+            y = y + this.titleTxt.height + 5;
+            this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'Press "N" to start the game');
+            this.startTxt.align = 'center';
+            this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
+            //            Legacy! fra Lars 
+            //            this.input.onDown.add(this.onDown, this);
+            key.onDown.add(this.startGame, this);
+        },
+        startGame: function () {
+            this.game.state.start('game');
+        },
+
+        //        Legacy! fra Lars
+        //        onDown: function () {
+        //              this.game.state.start('game');
+        //        }
+    };
+
+    window['pixione'] = window['pixione'] || {};
+    window['pixione'].Menu = Menu;
 
 }());
