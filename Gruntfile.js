fd6057b2015045f27b8e62cef808dea13b3b405e
andrethy <2809thy@gmail.com>
2015-05-05 12:17:15 +0200

    Fix for game.css load

--- Gruntfile.js
+++ Gruntfile.js
@@ -488,7 +488,7 @@ module.exports = function (grunt) {
         },
         files: {
           '<%= yeoman.client %>/index.html': [
-            '<%= yeoman.client %>/{app,components}/**/*.css'
+//            '<%= yeoman.client %>/{app,components}/**/*.css'
           ]
         }
       }
