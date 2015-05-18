0d0b0b521f359d4973220f86dcf57e36d56443fa
Kevin Anders Ørskov <kevin.2703@hotmail.com>
2015-05-18 10:48:33 +0200

    Soft delete done

--- server/api/highscore/highscore.model.js
+++ server/api/highscore/highscore.model.js
@@ -6,7 +6,8 @@ var mongoose = require('mongoose'),
 var HighscoreSchema = new Schema({
     name: String,
     score: Number,
-    date: {type: Date, default: Date.now}
+    date: {type: Date, default: Date.now},
+    isDeleted: Boolean
 });
 
 module.exports = mongoose.model('Highscore', HighscoreSchema);
--- server/config/seed.js
+++ server/config/seed.js
@@ -12,54 +12,70 @@ Highscore.find({}).remove(function() {
   Highscore.create({
     name : 'Anaconda',
     score : '1000',
-    date: "2015-03-24T11:23:22.048Z"
+    date: "2015-03-24T11:23:22.048Z",
+    isDeleted: false
   }, {
     name : 'Rambo',
     score : '900',
-      date: "2015-03-24T11:23:22.048Z"
+    date: "2015-03-24T11:23:22.048Z",
+    isDeleted: false
   }, {
     name : 'Conan',
     score : '800',
-      date: "2015-02-24T11:23:22.048Z"
+    date: "2015-02-24T11:23:22.048Z",
+    isDeleted: false
   },  {
     name : 'Kaj Børge',
-    score : '700'
+    score : '700',
+    isDeleted: false
   },{
     name : 'Lars Bilde',
-    score : '600'
+    score : '600',
+    isDeleted: false
   }, {
     name : 'Trillebør',
-    score : '500'
+    score : '500',
+    isDeleted: false
   }, {
     name : 'André',
-    score : '400'
+    score : '400',
+    isDeleted: false
   }, {
     name : 'Tuller',
-    score : '350'
+    score : '350',
+    isDeleted: false
   }, {
     name : 'Slikkepind',
-    score : '300'
+    score : '300',
+    isDeleted: false
   }, {
     name : 'Briller',
-    score : '270'
+    score : '270',
+    isDeleted: false
   }, {
     name : 'Træt af stole',
-    score : '250'
+    score : '250',
+    isDeleted: false
   }, {
     name : 'slik med',
-    score : '220'
+    score : '220',
+    isDeleted: false
   }, {
     name : 'Heldigt',
-    score : '200'
+    score : '200',
+    isDeleted: false
   }, {
     name : 'Igen',
-    score : '150'
+    score : '150',
+    isDeleted: false
   }, {
     name : '*raslen med slikpapir*',
-    score : '100'
+    score : '100',
+    isDeleted: false
   }, {
     name : 'Snøffel',
-    score : '50'
+    score : '50',
+    isDeleted: false
   });
 });
 User.find({}).remove(function() {
